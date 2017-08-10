c135344d023bfa0888a553548def01cf1724dea0
yarlou <YArlou@gomel.iba.by>
2017-08-10 21:15:33 +0300

    .gitignore
    dist removed

---
+++ dist/assets/libraries/font-resize.js
@@ -0,0 +1,7 @@
+var GlobalObject = {}
+GlobalObject.FontResize = function() {
+    var elements = document.getElementsByClassName('global-font-resize');
+    for (var i in elements) {
+        
+    }
+}
